5ddd604210dc3e7b657662edb9877e27d1831c66
lilyi <lilyli@qnap.com>
2017-04-19 18:02:46 +0800

    half

--- connect_db.py
+++ connect_db.py
@@ -8,28 +8,70 @@ Created on Tue Apr 18 15:52:41 2017
 # coding=UTF-8
 
 import MySQLdb
-
+import statistics
+import csv
+import smtplib
 try:
-  db = MySQLdb.connect("qnap.dev","root","root","yen_nas",charset='utf8')
-  sql = "SELECT * FROM `qtip_product_specs` WHERE `field_id` = 757"
-#  sql = "SELECT page_type, page_id, count(*) as total_votes FROM `page_helpful` where page_type='tutorial' group by `page_type`, `page_id` order by total_votes desc"
+#  db = MySQLdb.connect(host="10.8.2.125",user="marketing_query",passwd="WStFfFDSrzzdEQFW",db="web_analytics",charset='utf8')
+  db = MySQLdb.connect("qnap.dev","root","root","web_analytics",charset='utf8')
+#  sql = "SELECT * FROM `qtip_product_specs` WHERE `field_id` = 757"
+  sql = "SELECT page_type, page_id, count(*) as total_votes FROM `page_helpful` where page_type='tutorial' group by `page_id` order by total_votes desc"
+#SELECT page_id, count(*) as unhelpful_count FROM `page_helpful` WHERE `page_type` = 'tutorial' AND `page_id` IN (137,184,253,15,246) AND helpful = 0 group by page_id
 
   # 執行SQL statement
   cursor = db.cursor()
   cursor.execute(sql)
 
   # 撈取多筆資料
-  results = cursor.fetchall()
+  results = cursor.fetchall()   
 #  print(results)
   # 迴圈撈取資料
+  page_type = []
+  page_id = []
+  total_votes = []
+  L = []
+  d = {}
   for record in results: 
-      page_type = record[0]
-      page_id = record[1]
-      total_votes = record[2]
-      
-    
-      print ("%s, %s, %s" % (page_type, page_id, total_votes))
+      L.append(record[:])
+      d[str(record[1])] = record[0], record[2]
+      page_type.append(record[0])
+      page_id.append(record[1])
+      total_votes.append(record[2]) 
+  med = statistics.median(total_votes)
+  print(med)
+  pID = []
+  for i in L:
+      if i[2] >= med:
+         pID.append(str(i[1]))
+  print(pID, len(pID), len(d))
+  pID2 = ','.join(pID)
+  sql2 = "SELECT page_id, count(*) as unhelpful_count FROM `page_helpful` WHERE `page_type` = 'tutorial' AND `page_id` IN (%s) AND helpful = 0 group by page_id" % pID2
+  cursor.execute(sql2)
+  res = cursor.fetchall()
+  print(len(res))
+  print(d)
+  page_ID = []
+  unfelpful_count = []
+  percent = []
+  L2 = []
+  R = []
+  for i in res:
+      L2.append(i[:])
+      page_ID.append(str(i[0]))
+      unfelpful_count.append(i[1])
+      per = round(100*(i[1]/d[str(i[0])][1]),2)
+      percent.append(per)
+      R.append([str(i[0]), per])
+  top10 = sorted(R, key = lambda x : x[1], reverse=True)[:10]
+  print(top10)
 
+  with open('eggs.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['page_id','unhelpful (%)'])
+    writer.writerows(top10)
+  
+  
+    
   # 關閉連線
   db.close()
 
--- notify_tutorial_faq.py
+++ notify_tutorial_faq.py
@@ -4,31 +4,58 @@ Created on Mon Apr 17 16:33:18 2017
 
 @author: Lily
 """
-
 import smtplib
-fromaddr = 'lilyli@qnap.com'
-toaddrs  = 'lily60622@gmail.com'
-msg = "\r\n".join([
-  "From: lilyli@qnap.com",
-  "To: lily60622@gmail.com",
-  "Subject: Just a message",
-  "",
-  "Why, oh why"
-  ])
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.text import MIMEText
+#==============================================================================
+# fromaddr = 'lilyli@qnap.com'
+# toaddrs  = 'lily60622@gmail.com'
+# msg = "\r\n".join([
+#   "From: lilyli@qnap.com",
+#   "To: lily60622@gmail.com",
+#   "Subject: Just a message",
+#   "",
+#   "Why, oh why"
+#   ])
+#==============================================================================
+SUBJECT = "Email Data"
+EMAIL_FROM = 'lilyli@qnap.com'
+EMAIL_TO = ['lily60622@gmail.com', 'lilyli@qnap.com']
+msg = MIMEMultipart()
+msg['Subject'] = SUBJECT 
+msg['From'] = EMAIL_FROM
+msg['To'] = ', '.join(EMAIL_TO)
+text = "Hi,\n\nHere is the top10 unhelpful pages.\nFYI.\n"
+text1 = "Best Regards,\nLily, Li\n\nQNAP Systems, Inc.\n3F, No.22, Zhongxing Rd., Xizhi Dist., New Taipei City, 221, Taiwan\n Tel: 886-2-2641-2000 #11091"
+part = MIMEBase('application', "octet-stream")
+part1 = MIMEText(text1, 'plain')
+part.set_payload(open("eggs.csv", "rb").read())
+encoders.encode_base64(part)
+
+part.add_header('Content-Disposition', 'attachment; filename="eggs.csv"')
+
+msg.attach(part)
+msg.attach(part1)
+
 username = 'lilyli@qnap.com'
-password = 'lilyshea07'
+password = 'qnaplily07'
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.ehlo()
 server.starttls()
 server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
+#server.sendmail(fromaddr, toaddrs, msg)
+server.sendmail(EMAIL_FROM, EMAIL_TO, msg.as_string())
 server.quit()
-
-import MySQLdb
-
-try:
-  # 建立DB 連線資訊定設定中文編碼utf-8
-  db = MySQLdb.connect("qnap.dev","root","root","yen_nas",charset='utf8')
-
-except MySQLdb.Error as e:
-  print ("Error %d: %s" % (e.args[0], e.args[1]))
+print('sent!')
+#==============================================================================
+# import MySQLdb
+# 
+# try:
+#   # 建立DB 連線資訊定設定中文編碼utf-8
+#   db = MySQLdb.connect("qnap.dev","root","root","yen_nas",charset='utf8')
+# 
+# except MySQLdb.Error as e:
+#   print ("Error %d: %s" % (e.args[0], e.args[1]))
+#==============================================================================
