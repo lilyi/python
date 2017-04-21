ae62555ebfdbf1d84cee0846f95fbb2fd63b332e
lilyi <lilyli@qnap.com>
2017-04-21 17:37:16 +0800

    connected, added total_votes column, completed

--- connect_db.py
+++ connect_db.py
@@ -15,67 +15,55 @@ def main():
     type_name = ['tutorial', 'faq']
     for each in type_name:
         try:
-#            db = MySQLdb.connect(host="10.8.2.125",user="marketing_query",passwd="WStFfFDSrzzdEQFW",db="web_analytics")
-            
-#            db = MySQLdb.connect(host="10.8.2.125",user="marketing_query",passwd="WStFfFDSrzzdEQFW",db="web_analytics",charset='utf8')
-            db = MySQLdb.connect("qnap.dev","root","root","web_analytics")
-    #        sql = "SELECT * FROM `qtip_product_specs` WHERE `field_id` = 757"
+            db = MySQLdb.connect(host="10.8.2.125",user="marketing_query",passwd="WStFfFDSrzzdEQFW",db="web_analytics")
+#            db = MySQLdb.connect("qnap.dev","root","root","web_analytics")
+
             sql = "SELECT page_type, page_id, count(*) as total_votes FROM `page_helpful` where page_type='%s' group by `page_id` order by total_votes desc" % each
-    #        sql = "SELECT page_id, count(*) as unhelpful_count FROM `page_helpful` WHERE `page_type` = 'tutorial' AND `page_id` IN (137,184,253,15,246) AND helpful = 0 group by page_id"    
-    #        執行SQL statement
+#            sql = "SELECT page_id, count(*) as unhelpful_count FROM `page_helpful` WHERE `page_type` = 'tutorial' AND `page_id` IN (137,184,253,15,246) AND helpful = 0 group by page_id"    
+#            執行SQL statement
             cursor = db.cursor()
             cursor.execute(sql)        
-    #        撈取多筆資料
+#            撈取多筆資料
             all_results = cursor.fetchall()   
-            print(all_results)
-            print('\n')
-    #        迴圈撈取資料
-    #        page_type = []
-    #        page_id = []
             total_votes = []
             all_data = []
             all_dict = {}
             for record in all_results: 
                 all_data.append(record[:])
                 all_dict[str(record[1])] = record[0], record[2]
-    #            page_type.append(record[0])
-    #            page_id.append(record[1])
                 total_votes.append(record[2]) 
             med = statistics.median(total_votes)
-            print('med = ',med)
+#            print('med = ',med)
             all_pID = []
             for i in all_data:
                 if i[2] >= med:
                    all_pID.append(str(i[1]))
-    #        print(all_pID, len(all_pID), len(all_dict))
             all_pID2 = ','.join(all_pID)
             sql2 = "SELECT page_id, count(*) as unhelpful_count FROM `page_helpful` WHERE `page_type` = '%s' AND `page_id` IN (%s) AND helpful = 0 group by page_id" % (each, all_pID2)
             cursor.execute(sql2)
             unhelpful_results = cursor.fetchall()
-            print(unhelpful_results)
-            print('\n')
-            print(all_dict)
-            print('\n +++++')
+#            print('\n +++++')
             unhelpful_pID = []
             unfelpful_count = []
             percent = []
             unhelpful_data = []
-            result = []
+            result = []         
             for i in unhelpful_results:
                 unhelpful_data.append(i[:])
                 unhelpful_pID.append(str(i[0]))
                 unfelpful_count.append(i[1])
                 per = round(100*(i[1]/all_dict[str(i[0])][1]),2)
                 percent.append(per)
-                result.append([str(i[0]), per])
-            top10 = sorted(result, key = lambda x : x[1], reverse=True)[:10]
+                result.append([str(i[0]), per, all_dict[str(i[0])][1]])
+            top10 = sorted(result, key = lambda x : (x[1], x[2]), reverse=True)[:10]
+#            print(result)
 #            print(top10)
             
             with open('unhelpful_%s.csv' % each, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow(['page_id','unhelpful (%)'])
+                writer.writerow(['page_id','unhelpful (%)', 'total_votes'])
                 writer.writerows(top10)
-    #        關閉連線
+#            關閉連線
             db.close()
         
         except MySQLdb.Error as e:
